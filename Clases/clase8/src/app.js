c28d90e5129e2541554df81b1da16a1b8419e961
guidomidolo <gui.midolo@hotmail.com>
2023-08-01 02:17:12 -0300

    adelanto PE1

--- Clases/clase8/src/app.js
+++ Clases/clase8/src/app.js
@@ -1,8 +1,9 @@
 import express from "express";
 import petsRouter from "./routes/pets.router.js";
 import usersRouter from "./routes/users.router.js";
+import __dirname from "../utils.js";
 
-const port = 8080;
+const port = 8082;
 const app = express();
 
 app.use(express.json());
@@ -11,7 +12,7 @@ app.use(express.urlencoded({extended: true}));
 app.use("/api/users", usersRouter);
 app.use("/api/pets", petsRouter);
 
-app.use(express.static("public"));
+app.use(express.static(__dirname + "/public"));
 
 app.listen(port, () => {
     console.log("Servidor activo en el puerto: " + port);
